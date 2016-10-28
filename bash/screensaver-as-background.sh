0191db38896f27e67f4e52f8fee57ab080d9661a
Peter Lopez <peter@peterlopezweb.com>
2016-10-28 00:43:57 -0700

    Added misc Bash scripts for Mac OS apps
    - setup Sublime Text 2 'subl' command
    - set desktop background as screensaver

---
+++ bash/screensaver-as-background.sh
@@ -0,0 +1,2 @@
+# Sets Mac OS X desktop background as currently configured screensaver
+/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background
