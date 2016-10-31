8a876518474b1cddb60090f2a51efbdaaa7e974d
Peter Lopez <peter@peterlopezweb.com>
2016-10-31 00:16:50 -0700

    Added mysql script to manually clear cache tables in database

---
+++ mysql/clear-cache-drupal8.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE cache_bootstrap;
+TRUNCATE TABLE cache_config;
+TRUNCATE TABLE cache_container;
+TRUNCATE TABLE cache_data;
+TRUNCATE TABLE cache_default;
+TRUNCATE TABLE cache_menu;
+TRUNCATE TABLE cache_render;
+
