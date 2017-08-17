6d175080e16962773c20ad3e6c37d78e86b3374a
Peter Lopez <peterlopez@littlepassports.com>
2017-08-17 16:25:40 -0700

    revised bash script to start VM

--- bash/startvm.sh
+++ bash/startvm.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 VMNAME=$1
+if [ -z "$1" ]; then
+    echo "USAGE: sh startvm.sh {VM name}";
+    exit;
+fi
 
 running=1;
-running_msg="Virtual Machine '$VMNAME' currently running"
+running_msg="VM '$VMNAME' currently running"
 running_msg_shown=0;
 
 while [ $running -eq 1 ]
@@ -15,7 +19,7 @@ do
         while true; do
             read -p "Do you want to shutdown the VM? (y/n): " yn
             case $yn in
-                [Yy]* ) echo "shutting down VM $VMNAME"; VBoxManage controlvm local.lpdev.us acpipowerbutton; exit 0; break;;
+                [Yy]* ) echo "shutting down VM $VMNAME"; VBoxManage controlvm $VMNAME acpipowerbutton; exit 0; break;;
                 [Nn]* ) running=0; exit 0;;
                 * ) echo "Enter y (yes) or n (no)..";;
             esac
@@ -25,4 +29,4 @@ do
     sleep 1
 done
 
-VBoxManage startvm local.lpdev.us --type headless
+VBoxManage startvm $VMNAME --type headless
