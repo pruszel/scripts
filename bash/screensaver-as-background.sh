7cd4b5cf7be232cf7cf6c3b76a764193063c8d77
Peter Lopez <peter@peterlopez.me>
2020-03-19 13:51:33 -0700

    renamed osx directory

--- bash/screensaver-as-background.sh
+++
@@ -1,2 +0,0 @@
-# Sets Mac OS X desktop background as currently configured screensaver
-/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background
